7ea1286ef514b856cef5f732fcbf6b8a49a7cc66
LOSS98 <134796259+LOSS98@users.noreply.github.com>
2025-04-25 10:01:45 +0200

    :art: update navigation label and enhance video layout; add audio feedback for special profiles

--- modules/header.php
+++ modules/header.php
@@ -6,13 +6,13 @@
         <a href="index.php" class="logo-img"><img src="assets/img/logo-main.png" alt="Logo de la liste"><span>Los Barryachis</span></a>
         <ul>
             <li><a href="index.php" <?php if($nom_page == "Accueil"){ echo 'class="onPage"'; }?>>Accueil</a></li>
-            <li><a href="shotguns.php" <?php if($nom_page == "Shotguns"){ echo 'class="onPage"'; }?>>Shotguns</a></li>
+            <li><a href="shotguns.php" <?php if($nom_page == "Shotguns"){ echo 'class="onPage"'; }?>>Résultats des shotguns</a></li>
             <li><a href="programme.php" <?php if($nom_page == "Programme"){ echo 'class="onPage"'; }?>>Programme</a></li>
             <li><a href="teams.php" <?php if($nom_page == "Teams"){ echo 'class="onPage"'; }?>>Teams</a></li>
             <li><a href="promesses.php" <?php if($nom_page == "Promesses"){ echo 'class="onPage"'; }?>>Promesses</a></li>
         </ul>
         <div class="btn btn-primary" id="btn-taxi">
-            <a href="#" download>Mexigo <ion-icon name="car_outline"></ion-icon></a>
+            <a href="#" download>Mexigo <ion-icon name="car"></ion-icon></a>
         </div>
     </nav>
 </header>
